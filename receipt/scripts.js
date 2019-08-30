c9aded8b6feea2d2a68c0471cf0e33ddaab03472
Vladimir Shefer <quameu@gmail.com>
2019-08-31 01:51:39 +0300

    RC-Frontend Major reimplementation on vue.js

--- receipt/scripts.js
+++ receipt/scripts.js
@@ -1,24 +1,112 @@
-var vueReceiptTable = new Vue({
-  el: '#results',
+var app = new Vue({
+  el: '#application',
   data: {
     items: [],
+    form: {
+      fn: "",
+      fd: "",
+      fp: "",
+      summary: "",
+      date: new Date()
+    },
     meta: {}
+  },
+  computed: {
+    /**
+     * String representing date in format HH:MM
+     */
+    timeStr: {
+      get: function () {
+        let hour = this.form.date.getHours().toString().padStart(2, 0);
+        let minute = this.form.date.getMinutes().toString().padStart(2, 0);
+        return `${hour}:${minute}`;
+      },
+      set: function (str) {
+        if (str != null && str.match(/^\d{2}:\d{2}$/g)) {
+          let hour = str.substr(0, 2);
+          let minute = str.substr(3, 2);
+          this.form.date.setHours(hour, minute)
+        }
+      }
+    },
+    /**
+     * String representing date in format YYYY-MM-DD
+     */
+    dateStr: {
+      get: function () {
+        let date = this.form.date;
+        let year = date.getFullYear().toString();
+        let month = (date.getMonth() + 1).toString().padStart(2, 0);
+        let day = date.getDate().toString().padStart(2, 0);
+        return `${year}-${month}-${day}`;
+      },
+      set: function (str) {
+        if (str != null && str.match(/^\d{4}-\d{2}-\d{2}$/g)) {
+          let year = parseInt(str.substr(0, 4));
+          let month = parseInt(str.substr(5, 2)) - 1; // 0-based month required
+          let day = parseInt(str.substr(8, 2));
+          this.form.date.setFullYear(year, month, day)
+        }
+      }
+    },
+    /**
+     * String representing date in format YYYYMMDDTHHMM (T is just a letter)
+     */
+    datetime: {
+      get: function () {
+        let date = this.form.date;
+        let year = date.getFullYear().toString();
+        let month = (date.getMonth() + 1).toString().padStart(2, 0);
+        let day = date.getDate().toString().padStart(2, 0);
+        let hour = date.getHours().toString().padStart(2, 0);
+        let minute = date.getMinutes().toString().padStart(2, 0);
+        return `${year}${month}${day}T${hour}${minute}`;
+      },
+      set: function (str) {
+        if (str != null && str.match(/^\d{4}\d{2}\d{2}T\d{2}\d{2}$/g)) {
+          let year = parseInt(str.substr(0, 4));
+          let month = parseInt(str.substr(4, 2)) - 1;// 0-based month required
+          let day = parseInt(str.substr(6, 2));
+          let hour = parseInt(str.substr(str.length - 4, 2));
+          let minute = parseInt(str.substr(str.length - 2, 2));
+          this.form.date.setFullYear(year, month, day);
+          this.form.date.setHours(hour, minute);
+        }
+      }
+    }
+  },
+  methods: {
+    getQueryParameters: function () {
+      return `fn=${app.form.fn}&i=${app.form.fd}&fp=${app.form.fp}&s=${app.form.summary}&t=${app.datetime}`
+    },
+    setFormFromQueryParameters: function (parameters) {
+      this.form.fn = parameters.get("fn");
+      this.form.fd = parameters.get("i");
+      this.form.fp = parameters.get("fp");
+      this.form.summary = parameters.get("s");
+      this.datetime = parameters.get("t");
+    }
+  },
+  created: function () {
+    this.setFormFromQueryParameters(new URL(window.location.href).searchParams);
   }
-})
+});
 
+/**
+ * Serializes form and performs request to rest-api to fetch receipt data.
+ */
 const handleFormSubmit = function () {
-  let parameters = getFormParameters();
-  let myurl = "http://receipt.shefer.space/rest/get?" + parameters;
   $("#submit").attr('disabled', true);
   setTimeout(function () {
     $("#submit").attr('disabled', false);
   }, 5000);
   $.ajax({
-    url: myurl,
+    url: `/rest/get?${app.getQueryParameters()}`,
     context: document.body,
     success: function (data) {
       let answer = JSON.parse(data);
-      printToTable(answer);
+      app.items = answer.items;
+      app.meta = answer.meta;
       console.log(JSON.stringify(answer));
     },
     error: function (xhr) {
@@ -29,84 +117,11 @@ const handleFormSubmit = function () {
   });
 };
 
-const printToTable = function (data) {
-  vueReceiptTable.items = data.items;
-  vueReceiptTable.meta = data.meta;
-};
-
-const loadParameters = function () {
-  let url = new URL(window.location.href);
-  let parameters = url.searchParams;
-  $("input#fn").val(parameters.get("fn"));
-  $("input#i").val(parameters.get("i"));
-  $("input#fp").val(parameters.get("fp"));
-  $("input#s").val(parameters.get("s"));
-  var n = parameters.get("t").length;
-  var time = parameters.get("t").substr(n - 4, 2) + ":" + parameters.get("t").substr(n - 2, 2);
-  var date = parameters.get("t").substr(0, 4) + "-" + parameters.get("t").substr(4, 2) + "-" + parameters.get("t").substr(6, 2);
-  $("input#time").val(time);
-  $("input#date").val(date);
-};
-
-$(document).ready(function () {
-  setDefaultTime();
-  loadParameters();
-});
-
-const getFormParameters = function () {
-  let fn = document.querySelector('#fn');
-  let fd = document.querySelector('#i');
-  let fp = document.querySelector('#fp');
-  let s = document.querySelector('#s');
-  let time = document.querySelector('#time');
-  let dateF = document.querySelector('#date');
-  return "fn=" + fn.value +
-    "&i=" + fd.value +
-    "&fp=" + fp.value +
-    "&s=" + s.value +
-    "&t=" + dateF.value.split('-').join('') +
-    "T" + time.value.split(':').join('');
-};
-
+/**
+ * Serializes form and sets is as query to the address line
+ */
 const handleShare = function () {
-  if (dataIsValid()) {
-    let location = new URL(window.location.href);
-    let link = location.origin + location.pathname + "?" + getFormParameters();
-    history.pushState(null, null, link);
-  }
-};
-
-const setDefaultTime = function () {
-  var timeField = document.querySelector('#time');
-  var dateField = document.querySelector('#date');
-  var date = new Date();
-  timeField.value =
-    (date.getHours().toString().length < 2 ? ("0" + date.getHours().toString()) : date.getHours().toString()) +
-    ':' + (date.getMinutes().toString().length < 2 ? ("0" + date.getMinutes().toString()) : date.getMinutes().toString());
-  dateField.value =
-    date.getFullYear().toString() +
-    '-' + (date.getMonth() + 1).toString().padStart(2, 0) +
-    '-' + date.getDate().toString().padStart(2, 0);
-};
-
-const dataIsValid = function () {
-  let parameters = ['#fn', '#i', '#fp', '#s'];
-  var checked = 0;
-  for (let i in parameters) {
-    if (document.querySelector(parameters[i]).value.match(/^\d+$/g)) {
-      checked += 1;
-    }
-  }
-  var time = document.querySelector('#time').value.split(':').join('');
-  if (time[0] < "3" && time[2] < "6" && time.match(/^\d+$/g)) {
-    checked += 1;
-  } else {
-    alert("Проверьте значение времени!");
-  }
-  if (checked == 5) {
-    return true;
-  } else {
-    alert("Проверьте правильность введенных данных! Значения полей должны содержать только цифры!");
-    return false;
-  }
+  let url = new URL(window.location.href);
+  let link = url.origin + url.pathname + "?" + app.getQueryParameters();
+  window.history.pushState(null, null, link);
 };
