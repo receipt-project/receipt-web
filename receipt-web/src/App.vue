1c44e0223fb1dfe9415e4ed6013ce19d93f2f27e
Vladimir Shefer <quameu@gmail.com>
2019-12-29 00:13:35 +0300

    RCW-0 Rename navbar toggler

--- receipt-web/src/App.vue
+++ receipt-web/src/App.vue
@@ -1,11 +1,11 @@
 <template>
     <div id="app">
         <nav class="navbar navbar-expand-lg navbar-expand-md navbar-dark bg-dark" style="margin-bottom: 20px;">
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01"
-                    aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler"
+                    aria-controls="navbarToggler" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
-            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+            <div class="collapse navbar-collapse" id="navbarToggler">
                 <a class="navbar-brand" href="/">Receipt</a>
                 <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
                     <HeaderItem target-route="/test" text="Test"/>
@@ -19,10 +19,6 @@
 </template>
 
 <script>
-    // import HelloWorld from './components/HelloWorld.vue'
-    // import ReceiptCard from "./components/ReceiptCard";
-    // import ReceiptMeta from "./components/ReceiptMeta";
-
     import HeaderItem from "./components/HeaderItem";
 
     export default {
