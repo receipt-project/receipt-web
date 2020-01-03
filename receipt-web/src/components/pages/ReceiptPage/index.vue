2f61b07c1e757475eca7ead7d614a783282a6ae2
Vladimir Shefer <quameu@gmail.com>
2020-01-04 00:06:17 +0300

    RCW-0 Add input for receiptId selection

--- receipt-web/src/components/pages/ReceiptPage/index.vue
+++ receipt-web/src/components/pages/ReceiptPage/index.vue
@@ -1,12 +1,30 @@
 <template>
-    <div class="row">
-        <div class="col-12">
-            <p v-if="notFound">
-                Receipt #{{receiptId}} was not found
-            </p>
-            <receipt-items v-bind:items="items"/>
-            <div style="margin-bottom: 20px"></div>
-            <receipt-meta v-bind:meta="meta"/>
+    <div class="container">
+        <div class="row">
+            <div class="col-12 col-sm-8">
+                <form class="form-inline" v-on:submit.prevent>
+                    <label class="sr-only" for="ReceiptPageReceiptIdInput">Name</label>
+                    <input type="text"
+                           class="form-control mb-2 mr-sm-2"
+                           id="ReceiptPageReceiptIdInput"
+                           v-model="receiptId"
+                           placeholder="Receipt ID">
+                </form>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12 col-sm-8">
+                <p v-if="notFound">
+                    Receipt #{{receiptId}} was not found
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <receipt-items v-bind:items="items"/>
+                <div style="margin-bottom: 20px"></div>
+                <receipt-meta v-bind:meta="meta"/>
+            </div>
         </div>
     </div>
 </template>
@@ -22,32 +40,48 @@
         data: function () {
             return {
                 items: [],
-                meta: {},
-                notFound: false
+                meta: {}
+            }
+        },
+        computed: {
+            notFound: function () {
+                return this.receiptId && Object.keys(this.meta).length === 0;
             }
         },
         components: {
             ReceiptItems,
             ReceiptMeta
         },
+        watch: {
+            receiptId: function (newValue, oldValue) {
+                if (newValue && newValue !== oldValue) {
+                    this.$router.replace(`/receipt/${newValue}`);
+                    this.reloadReceipt();
+                }
+            }
+        },
+        methods: {
+            reloadReceipt: function () {
+                axios
+                        .put("/api/receipts", {ids: [this.receiptId]})
+                        .then(result => {
+                            let data = result.data;
+                            if (data.length > 0) {
+                                this.meta = data[0];
+                            } else {
+                                this.meta = {};
+                            }
+                        });
+
+                axios
+                        .put("/api/items", {receiptIds: [this.receiptId]})
+                        .then(result => {
+                            this.items = result.data;
+                        });
+            }
+        },
         created: function () {
-            axios
-                    .put("/api/receipts", {ids: [this.receiptId]})
-                    .then(result => {
-                        let data = result.data;
-                        if (data.length > 0) {
-                            this.meta = data[0];
-                        } else {
-                            this.notFound = true;
-                        }
-                    });
-
-            axios
-                    .put("/api/items", {receiptIds: [this.receiptId]})
-                    .then(result => {
-                        this.items = result.data;
-                    });
-
+            this.reloadReceipt();
         }
     }
 </script>
