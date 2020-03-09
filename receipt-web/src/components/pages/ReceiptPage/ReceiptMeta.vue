857e32c2dd18edd2e88408f0f925ffbad50c2d99
Vladimir Shefer <quameu@gmail.com>
2020-03-09 14:09:40 +0300

    Add readable keys of receipt meta

--- receipt-web/src/components/pages/ReceiptPage/ReceiptMeta.vue
+++ receipt-web/src/components/pages/ReceiptPage/ReceiptMeta.vue
@@ -25,12 +25,22 @@
                 for (let key in meta) {
                     if (key === "date") {
                         result["Дата"] = moment(meta["date"], "YYYY-MM-DD'T'HH:mm:ss").format("DD.MM.YYYY HH:mm");
+                    } else if (key === "id") {
+                        result["Идентификатор"] = meta["id"];
                     } else if (key === "place") {
                         result["Магазин"] = meta["place"];
                     } else if (key === "provider") {
                         result["Провайдер"] = meta["provider"];
                     } else if (key === "sum") {
                         result["Сумма"] = meta["sum"];
+                    } else if (key === "fn") {
+                        result["Фискальный накопитель"] = meta["fn"];
+                    } else if (key === "fd") {
+                        result["Номер фискального документа"] = meta["fd"];
+                    } else if (key === "fp") {
+                        result["Фискальный признак"] = meta["fp"];
+                    } else if (key === "status") {
+                        result["Статус чека"] = meta["status"];
                     } else {
                         result[key] = meta[key];
                     }
