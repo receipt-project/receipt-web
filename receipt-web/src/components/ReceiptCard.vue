52dc686f0c36e66d2b97854429f524c43b111e55
Dmitriy Petrov <dartwen@yandex.ru>
2020-05-04 01:07:42 +0300

    turned the card over

--- receipt-web/src/components/ReceiptCard.vue
+++ receipt-web/src/components/ReceiptCard.vue
@@ -1,18 +1,16 @@
 <template>
     <div class="card p-2 h-100" :class="cardStyle">
         <div class="row no-gutters h-100">
-            <div class="col-md-7">
+            <div class="col-md-12">
                 <div class="card-body d-md-flex flex-md-column justify-content-md-between h-100">
                     <div class="text-muted">
                         <span>{{ date.format("Do MMMM YYYY HH:mm") }}</span>
                         <p>{{ receipt.place }}</p>
                     </div>
                     <p class="card-text">{{ receipt.sum }} рублей.</p>
-                </div>
-            </div>
-            <div class="col-md-5 d-md-flex align-items-md-center btn-card-link">
-                <div class="card-body d-md-flex justify-content-md-center">
-                    <a :href="href" class="btn btn-primary">Открыть</a>
+                    <div class="btn-card-link d-md-flex justify-content-md-start">
+                        <a :href="href" class="btn btn-primary">Открыть</a>
+                    </div>
                 </div>
             </div>
         </div>
