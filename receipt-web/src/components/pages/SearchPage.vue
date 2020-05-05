288b8e422e7f73849cf273fa6568393a305d7bbd
Dmitriy Petrov <dartwen@yandex.ru>
2020-05-05 23:02:33 +0300

    entering digits only in the FN, FP, and FD

--- receipt-web/src/components/pages/SearchPage.vue
+++ receipt-web/src/components/pages/SearchPage.vue
@@ -7,30 +7,38 @@
                         <legend>Проверка чека</legend>
                         <div class="form-group">
                             <label for="fn">ФН, Фискальный номер</label>
-                            <input type="text" class="form-control" id="fn" v-model="form.fn">
+                            <input type="text" class="form-control" id="fn" v-model="form.fn" @keypress="enterNumber" />
                         </div>
                         <div class="form-group">
                             <label for="fd">ФД, Фискальный документ</label>
-                            <input type="text" class="form-control" id="fd" v-model="form.fd">
+                            <input type="text" class="form-control" id="fd" v-model="form.fd" @keypress="enterNumber" />
                         </div>
                         <div class="form-group">
                             <label for="fp">ФП, Фискальный признак</label>
-                            <input type="text" class="form-control" id="fp" v-model="form.fp">
+                            <input type="text" class="form-control" id="fp" v-model="form.fp" @keypress="enterNumber" />
                         </div>
                         <div class="form-group">
                             <label for="s">Сумма</label>
-                            <input type="text" class="form-control" id="s" placeholder="0.00" v-model="form.summary">
+                            <input type="text" class="form-control" id="s" placeholder="0.00" v-model="form.summary" />
                         </div>
                         <div class="form-group">
                             <label for="time">Время</label>
-                            <input type="text" class="form-control" id="time" v-model="timeField">
-                            <input type="date" class="form-control" id="date" v-model="dateField">
+                            <input type="text" class="form-control" id="time" v-model="timeField" />
+                            <input type="date" class="form-control" id="date" v-model="dateField" />
                         </div>
                         <div class="form-group">
-                            <input type="button" class="btn btn-primary btn-block" value="Отправить" id="submit"
-                                   v-on:click="handleFormSubmit()">
-                            <input type="button" class="btn btn-secondary btn-block" value="Поделиться" id="share"
-                                   v-on:click="handleShare()">
+                            <input type="button"
+                                   class="btn btn-primary btn-block"
+                                   value="Отправить"
+                                   id="submit"
+                                   @click="handleFormSubmit()"
+                            />
+                            <input type="button"
+                                   class="btn btn-secondary btn-block"
+                                   value="Поделиться"
+                                   id="share"
+                                   @click="handleShare()"
+                            />
                         </div>
                     </fieldset>
                 </form>
@@ -40,7 +48,7 @@
 </template>
 
 <script>
-    import moment from 'moment'
+    import moment from "moment";
     import axios from "axios";
 
     export default {
@@ -54,18 +62,17 @@
                     summary: "",
                     date: moment()
                 }
-            }
+            };
         },
         computed: {
-
             /**
              * String representing date in format HH:MM
              */
             timeField: {
-                get: function () {
+                get() {
                     return this.form.date.format("HH:mm");
                 },
-                set: function (str) {
+                set(str) {
                     if (str != null && str.match(/^\d{2}:\d{2}$/g)) {
                         this.form.date.set("hour", str.substr(0, 2));
                         this.form.date.set("minute", str.substr(3, 2));
@@ -77,37 +84,39 @@
              * String representing date in format YYYY-MM-DD
              */
             dateField: {
-                get: function () {
+                get() {
                     return this.form.date.format("YYYY-MM-DD");
                 },
-                set: function (str) {
+                set(str) {
                     if (str != null && str.match(/^\d{4}-\d{2}-\d{2}$/g)) {
                         this.form.date.set("year", parseInt(str.substr(0, 4)));
                         this.form.date.set("month", parseInt(str.substr(5, 2)) - 1);
                         this.form.date.set("date", parseInt(str.substr(8, 2)));
                     }
                 }
-            },
-
+            }
         },
         methods: {
+            enterNumber($event) {
+                const keyCode = $event.keyCode ? $event.keyCode : $event.which;
+                if (keyCode < 48 || keyCode > 57) {
+                    $event.preventDefault();
+                }
+            },
             handleShare() {
                 // TODO
             },
             handleFormSubmit() {
                 axios.post("/api/create", {
-                    date: this.form.date.format("YYYYMMDDTHHmm"),
-                    fn: this.form.fn,
-                    fd: this.form.fd,
-                    fp: this.form.fp,
-                    sum: parseFloat(this.form.summary),
-                })
-                        .then(response => this.$router.push(`/receipt/${response.data}`))
+                        date: this.form.date.format("YYYYMMDDTHHmm"),
+                        fn: this.form.fn,
+                        fd: this.form.fd,
+                        fp: this.form.fp,
+                        sum: parseFloat(this.form.summary)
+                    }).then(response => this.$router.push(`/receipt/${response.data}`));
             }
         }
-    }
+    };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
